8ba4c463a9dd2ea7fe9e03ebdb475c459c1e47a8
joji <56271473+jojiku@users.noreply.github.com>
2024-10-23 09:06:08 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,18 @@
 
+from .audio import AudioProcessor
+from .exceptions import (
+    TTSError,
+    TTSInitializationError,
+    TTSGenerationError,
+    Live2DError,
+    Live2DConnectionError
+)
+
+__all__ = [
+    "AudioProcessor",
+    "TTSError",
+    "TTSInitializationError",
+    "TTSGenerationError",
+    "Live2DError",
+    "Live2DConnectionError"
+]
