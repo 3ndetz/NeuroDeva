86b6194be84738499b7e9bf1c7acb8d11918a716
joji <56271473+jojiku@users.noreply.github.com>
2024-10-23 09:08:16 +0300

    Update formatters.py

--- tts/formatters.py
+++ tts/formatters.py
@@ -1,2 +1,5 @@
+import html
+
 def format_text_for_tts(text: str, rate: str = "medium", pitch: str = "medium") -> str:
-    return f'<speak><prosody rate="{rate}" pitch="{pitch}">{text}</prosody></speak>'
+    escaped_text = html.escape(text)
+    return f'<speak><prosody rate="{rate}" pitch="{pitch}">{escaped_text}</prosody></speak>'
